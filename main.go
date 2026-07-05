96890a29787b06ae3e8d5a79a6bec6540e4322ae
nobodysGitHub <91459536+nobodysGitHub@users.noreply.github.com>
2026-07-05 19:22:32 +0200

    html form

--- main.go
+++ main.go
@@ -6,10 +6,21 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", greetUser)
+	http.HandleFunc("/greet", greetUser)
+	http.HandleFunc("/", showForm)
 	http.ListenAndServe(":8080", nil)
 }
 
+func showForm(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Set("Content-Type", "text/html")
+	html := `
+		<form action="/greet" method="GET">
+			<input type="text" name="name" placeholder="Enter your name">
+			<button type="submit">Greet me</button>
+		</form>
+	`
+	fmt.Fprintln(writer, html)
+}
 func greetUser(writer http.ResponseWriter, request *http.Request) {
 	name := request.URL.Query().Get("name")
 
